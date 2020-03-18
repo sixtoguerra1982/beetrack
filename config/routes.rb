b51b320a44db27b6ed8d7ef8105b8c52b55e5f4f
Sixto Guerra <sixto.guerra1982@gmail.com>
2020-03-18 09:15:44 -0300

    end point create vehicle

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get 'vehicles/create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :vehicles
+  resources :vehicles, only: [:create]
 end
