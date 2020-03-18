30ac49d4ecab41364c6ec3158038cb5bd17776fb
Sixto Guerra <sixto.guerra1982@gmail.com>
2020-03-18 13:08:11 -0300

    GpsModel Controller Create

--- app/controllers/gps_models_controller.rb
+++ app/controllers/gps_models_controller.rb
@@ -1,15 +1,26 @@
 class GpsModelsController < ActionController::API
   def create
+
+
+
   	vehicle_identifier = params[:vehicle_identifier]
   	if ! vehicle_identifier.nil?
-  		@v = Vehicle.where(identifier: vehicle_identifier).first_or_create
-  		render json: { identifier: @v.identifier }
+  		@v = Vehicle.where(identifier: vehicle_identifier).first_or_create	
   	end
+
+  	@gps = GpsModel.new(gpsmodel_params)
+  	@gps.vehicle = @v
+
+  	if @gps.save
+		render json: {gps: @gps , vehicle: @v}, status: :created
+	else
+		render json: @gps.errors, status: :unprocessable_entity
+	end
+
   end
 
   private
     def gpsmodel_params
-      params.require(:vehicle).permit(:identifier)
+      params.permit(:latitude, :longitude, :sent_at)
     end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :vehicles, only: [:create]
-  post 'api/v1/gps', to: 'gps_models#create'
+  post 'api/v1/gps/', to: 'gps_models#create'
 end
